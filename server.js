bdd0aecf5b2a6f891bee1c12db1f99d88fbf31fd
Maana908 <maana@live.unc.edu>
2021-11-29 16:25:32 -0500

    4th commit

--- server.js
+++ server.js
@@ -32,12 +32,14 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {	
-	req.params.id(1);
-	const stmt = db.prepare("SELECT * FROM userinfo").get();
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-
+app.patch("/app/update/user/:id", (req, res) => {	
+	const stmt = db.prepare("UPDATE * FROM userinfo WHERE id = ?").run(req.params.id);
+	res.status(200).json(stmt);
+});
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
 // Default response for any other request
